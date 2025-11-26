231a367610e4005eeaeed580778b387ab1061a40
SergioGomezAlvarez <sgmichielr@gmail.com>
2025-11-26 14:13:35 +0100

    Initialize First Plugin with basic structure and silence index file

---
+++ first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: First Plugin
+ * Description: This is my first WordPress plugin.
+ * Version: 1.0.0
+ * Author: Sergio
+ * Author URI: https://sergiogomezalvarez.github.io/Portfolio/index.html
+ */
+?>
